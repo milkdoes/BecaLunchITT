48895a176dcbefc5456d9ad1efb2518f6e7fa928
Salcedo Jose <josemasal@gmail.com>
2016-10-31 23:04:43 -0700

    Envio de correo exitoso.

--- php/Contacto.php
+++ php/Contacto.php
@@ -1,21 +1,31 @@
 <?php
 // Declaracion de constantes.
+// Correo al cual enviar el mensaje.
 define("DESTINO", "josemasal@gmail.com");
+// Mensaje para indicar al usuario que se va a redireccionar a la pagina anterior.
+define("SALIR", " Redireccionando... <script>setTimeout(\"location.href = '../contact.html';\", 3000);</script>");
 
+// Si los valores estan definidos y no son vacios, continuar.
 if (
     (isset($_POST["nombre"]) && !empty($_POST["nombre"]))
     && (isset($_POST["email"]) && !empty($_POST["email"]))
 ) {
+    // Declaracion de variables para el correo a enviar.
     $nombre = $_POST["nombre"];
     $correoInicial = $_POST["email"];
     $destino = DESTINO;
     $asunto = "Contactar a " . $nombre . "\n";
     $mensaje = $_POST["mensaje"];
-    $emailheader = "From: " . $nombre . "<" .  . ">\r\n";
+    $emailheader = "From: " . $nombre . "<" . $correoInicial . ">\r\n";
+
+    // Enviar correo y desplegar mensaje si el envio fue exitoso o no.
+    // De igual manera, redireccionar.
+    mail($destino, $asunto, $mensaje, $emailheader)
+        or die("Lo sentimos, tu mensaje no fue enviado correctamente. <br />Intentalo de nuevo." . SALIR);
     
-    mail($destino, $asunto, $mensaje, $emailheader) or die("Lo sentimos, tu solicitud no ha sido enviada. <br />Intentalo de nuevo.");
-    echo utf8_decode(utf8_encode("Tu consulta fue enviada correctamente!"));
+    echo utf8_decode(utf8_encode("Muchas gracias. Nos comunicaremos contigo lo mas pronto posible." . SALIR));
 } else {
+    // Si el nombre y/o el email estan vacios, indicarselo al usuario.
     if( empty($_POST["nombre"]) || empty($_POST["nombre"]) == true ) {
         echo utf8_encode("Por favor, indica tu nombre.");
     }
@@ -23,5 +33,8 @@ if (
     if( empty($_POST["email"]) || empty($_POST["email"]) == true ) {
         echo utf8_encode("Por favor, indica tu email.");
     }
+
+    // Redireccionar.
+    echo SALIR;
 }
 ?>
