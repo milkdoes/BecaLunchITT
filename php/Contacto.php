ff000375b2225ec6979b0c77c0a5c8cc6764ed91
Salcedo Jose <josemasal@gmail.com>
2016-10-31 22:33:20 -0700

    Agregado metodo para enviar correo a quien sea el encargado de administrar correos.

--- php/Contacto.php
+++ php/Contacto.php
@@ -1,8 +1,27 @@
 <?php
 // Declaracion de constantes.
-define("RUTA", "\"../contact.html\"");
-define("TIMEOUT", "\"3000\"");
+define("DESTINO", "josemasal@gmail.com");
 
-echo "Muchas gracias! Nos contactaremos contigo despues de un tiempo. Redireccionando....";
-      echo "<script>setTimeout(\"location.href = '../contact.html';\", 3000);</script>";
+if (
+    (isset($_POST["nombre"]) && !empty($_POST["nombre"]))
+    && (isset($_POST["email"]) && !empty($_POST["email"]))
+) {
+    $nombre = $_POST["nombre"];
+    $correoInicial = $_POST["email"];
+    $destino = DESTINO;
+    $asunto = "Contactar a " . $nombre . "\n";
+    $mensaje = $_POST["mensaje"];
+    $emailheader = "From: " . $nombre . "<" .  . ">\r\n";
+    
+    mail($destino, $asunto, $mensaje, $emailheader) or die("Lo sentimos, tu solicitud no ha sido enviada. <br />Intentalo de nuevo.");
+    echo utf8_decode(utf8_encode("Tu consulta fue enviada correctamente!"));
+} else {
+    if( empty($_POST["nombre"]) || empty($_POST["nombre"]) == true ) {
+        echo utf8_encode("Por favor, indica tu nombre.");
+    }
+
+    if( empty($_POST["email"]) || empty($_POST["email"]) == true ) {
+        echo utf8_encode("Por favor, indica tu email.");
+    }
+}
 ?>
